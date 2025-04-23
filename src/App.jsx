efc78689c2c25177c12b75eb0b01303e2e4114fb
Marc Munene <marcmunene104@gmail.com>
2025-04-23 20:30:41 +0300

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { LuInstagram } from "react-icons/lu";
+import { FaGithub } from "react-icons/fa";
 const App = () => {
   return (
     <div>
@@ -9,14 +11,35 @@ const App = () => {
           <ul>
             <li>About</li>
             <li>Projects</li>
-            <li>Experience</li> 
+            <li>Experience</li>
           </ul>
           <div>
-            
+            <LuInstagram />
+            <FaGithub />
           </div>
-
         </nav>
+        About Projects
       </header>
+
+      <section id="hero">
+        <div>
+          <div>
+            <img src="/src/assets/comp.jpg" alt="Computer" />
+          </div>
+          <div>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Doloribus quae minus autem vitae, cum maiores, iusto numquam
+              quidem sed unde quibusdam ex laudantium doloremque eos aut tempore
+              ullam nihil non? Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Soluta, at dolorum ipsum consequatur aliquam
+              placeat itaque? Nesciunt placeat voluptates molestiae magnam.
+              Eligendi inventore minus placeat velit et numquam eaque
+              temporibus!
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
