a9faea248f0f9bfd5818eba7f9567eec1ee5b9c1
Marc Munene <marcmunene104@gmail.com>
2025-04-24 01:53:54 +0300

    UI

--- src/App.jsx
+++ src/App.jsx
@@ -98,7 +98,7 @@ const App = () => {
                   <span className="text-pink-500">&lt;/p&gt;</span>
                 </p>
 
-                <div className="mt-6 mb-6">
+                <div className="mt-6 mb-3">
                   <button className="border border-cyan-500 text-cyan-500 hover:bg-cyan-500/10 px-4 py-2 sm:px-6 sm:py-3 rounded-lg cursor-pointer transition-all duration-300">
                     Contact Me?
                   </button>
@@ -109,7 +109,7 @@ const App = () => {
         </section>
 
         {/* About section */}
-        <section id="about" className=" py-8 md:py-12">
+        <section id="about" className="py-8 md:py-12">
           <div className="mx-auto max-w-6xl">
             <div className="py-4 px-6 flex-col md:flex-row items-center gap-8 lg:gap-12 ">
               {/* section title */}
