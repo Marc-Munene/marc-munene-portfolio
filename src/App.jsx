5fbd43d65e8928f3b436931d9b986c061cb13405
Marc Munene <marcmunene104@gmail.com>
2025-04-23 20:20:37 +0300

    react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 const App = () => {
-  return 
-  <div>
-    <header>
-      <div>
-         
-      </div>
-    </header>
-  </div>;
+  return (
+    <div>
+      <header>
+        <nav>
+          <div>
+            <img src="/public/munene.dev2.png" alt="logo" />
+          </div>
+          <ul>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Experience</li> 
+          </ul>
+          <div>
+            
+          </div>
+
+        </nav>
+      </header>
+    </div>
+  );
 };
 
 export { App };
- 
