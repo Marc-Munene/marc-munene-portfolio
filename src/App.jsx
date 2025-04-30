1e6a660190405c35f93b3bfea348e2b0425ebd72
Marc Munene <marcmunene104@gmail.com>
2025-04-30 22:01:08 +0300

    links / projects update

--- src/App.jsx
+++ src/App.jsx
@@ -291,36 +291,6 @@ const App = () => {
                   </div>
                 </div>
 
-                {/*Hangover football  */}
-
-                <div className=" rounded-xl shadow-md overflow-hidden transition-transform hover:scale-[1.02] hover:shadow-lg">
-                  <div className="relative">
-                    <img
-                      className="w-full h-48 md:h-56 object-cover"
-                      src="/hangover.png"
-                      alt="Hangover football website"
-                    />
-                  </div>
-                  <div className="p-5">
-                    <h3 className="text-lg md:text-xl font-semibold text-gray-800 mb-2">
-                      Hangover Football Academy
-                    </h3>
-                    <p className=" text-sm md:text-base mb-4">
-                      A football club website, entailing every detail of the
-                      club, from the partnersgym, field and partners.
-                    </p>
-                    <div className="flex justify-end">
-                      <a
-                        href="https://hangover-football-club.vercel.app/"
-                        target="blank"
-                      >
-                        <button className="bg-cyan-500 hover:bg-cyan-600 dark:bg-cyan-600 dark:hover:bg-cyan-700 text-white font-medium py-2 px-4 rounded-lg transition-all duration-300 shadow-sm hover:shadow-md transform hover:-translate-y-0.5 cursor-pointer">
-                          View Demo
-                        </button>
-                      </a>
-                    </div>
-                  </div>
-                </div>
                 <div className=" rounded-xl shadow-md overflow-hidden transition-transform hover:scale-[1.02] hover:shadow-lg">
                   <div className="relative">
                     <img
@@ -364,17 +334,12 @@ const App = () => {
 
         {/* Footer */}
 
-        <footer
-          id="contact"
-          className=" py-12 px-4 sm:px-6 lg:px-8 bg-gray-50"
-        >
+        <footer id="contact" className=" py-12 px-4 sm:px-6 lg:px-8 bg-gray-50">
           <div className="max-w-6xl mx-auto ">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
               {/* Contact Info */}
               <div className="space-y-4">
-                <h3 className="text-xl font-semibold">
-                  Get In Touch
-                </h3>
+                <h3 className="text-xl font-semibold">Get In Touch</h3>
                 <div className="space-y-2">
                   <div className="flex items-center">
                     <svg
@@ -413,9 +378,7 @@ const App = () => {
                         d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                       />
                     </svg>
-                    <span className="">
-                      +254 745 851 127
-                    </span>
+                    <span className="">+254 745 851 127</span>
                   </div>
                   <div className="flex items-center">
                     <svg
@@ -438,18 +401,14 @@ const App = () => {
                         d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                       />
                     </svg>
-                    <span className="">
-                      Nairobi, Kenya
-                    </span>
+                    <span className="">Nairobi, Kenya</span>
                   </div>
                 </div>
               </div>
 
               {/* Quick Links */}
               <div className="space-y-4">
-                <h3 className="text-xl font-semibold">
-                  Quick Links
-                </h3>
+                <h3 className="text-xl font-semibold">Quick Links</h3>
                 <ul className="space-y-2">
                   <li>
                     <a
@@ -488,9 +447,7 @@ const App = () => {
 
               {/* Social Media */}
               <div className="space-y-4">
-                <h3 className="text-xl font-semibold">
-                  Connect With Me
-                </h3>
+                <h3 className="text-xl font-semibold">Connect With Me</h3>
                 <div className="flex space-x-4">
                   <a
                     href="https://github.com/Marc-Munene"
@@ -509,7 +466,7 @@ const App = () => {
                     <BsInstagram className="h-6 w-6" />
                   </a>
                   <a
-                    href="https://www.linkedin.com/"
+                    href="https://www.linkedin.com/in/marc-munene-628a43362/"
                     target="_blank"
                     rel="noopener noreferrer"
                     className=" hover:text-blue-600 transition-colors"
@@ -521,21 +478,6 @@ const App = () => {
                       xmlns="http://www.w3.org/2000/svg"
                     >
                       <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
-                    </svg>
-                  </a>
-                  <a
-                    href="https://twitter.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className=" hover:text-blue-400 transition-colors"
-                  >
-                    <svg
-                      className="h-6 w-6"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
                     </svg>
                   </a>
                 </div>
@@ -559,7 +501,6 @@ const App = () => {
                 &copy; {new Date().getFullYear()} Marc Munene. All rights
                 reserved.
               </p>
-              
             </div>
           </div>
         </footer>
