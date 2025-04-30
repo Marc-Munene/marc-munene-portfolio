b81737c2b185bb31037825894e8cb8e66c5de795
Marc Munene <marcmunene104@gmail.com>
2025-04-30 21:23:05 +0300

    work experience section

--- src/App.jsx
+++ src/App.jsx
@@ -107,7 +107,7 @@ const App = () => {
                     className="inline-block border  hover:bg-gray-100  px-4 py-2 sm:px-5 sm:py-2.5 md:px-5 md:py-2.5 text-sm sm:text-base md:text-base rounded-lg transition-all duration-300 hover:shadow-md transform hover:-translate-y-0.5 hover:scale-[1.02]"
                   >
                     <span className="flex items-center gap-3">
-                      <IoIosContact size={24} /> Contact
+                      <IoIosContact size={24} /> Get in Touch
                     </span>
                   </a>
                 </div>
@@ -150,6 +150,67 @@ const App = () => {
                     className="rounded-lg shadow-xl w-full max-w-xs sm:max-w-sm md:w-80 lg:w-96 h-auto object-cover border-4 border-none"
                     loading="lazy"
                   />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="work-experience" className="  md:py-10">
+          <div className="mx-auto  py-8 px-4 max-w-6xl">
+            <div className="py-4 px-6 flex-col md:flex-row items-center gap-8 lg:gap-12 ">
+              {/* section title */}
+              <div className="flex justify-center sm:justify-center md:justify-start lg:justify-start items-center mb-5">
+                <h1 className="border-transparent py-1 px-2 rounded-md bg-gray-200 text-sm sm:text-base md:text-lg lg:text-xl w-auto sm:w-[8%] min-w-max">
+                  Work Experience
+                </h1>
+              </div>
+              <div className="flex flex-col md:flex-row items-center gap-5 lg:gap-10">
+                <div className="mb-10">
+                  <p className="font-bold text-xl leading-normal mb-3">
+                    Work Experience
+                  </p>
+
+                  <div className="flex flex-col gap-4">
+                    <div className="flex select-none">
+                      {/* Company Logo */}
+                      <div className="flex-none">
+                        <img
+                          className="w-12 h-12 rounded-full border-2 border-gray-300"
+                          src="/usalama.jpg"
+                          alt="Usalama Logo"
+                        />
+                      </div>
+
+                      {/* Text Info */}
+                      <div className="flex-grow ml-4 flex flex-col cursor-pointer">
+                        <div className="flex flex-col">
+                          <div className="w-full flex justify-between">
+                            <p className="font-semibold text-xs sm:text-sm text-black">
+                              Junior Software Developer Intern
+                            </p>
+                            <p className="text-xs sm:text-sm text-gray-600 whitespace-nowrap">
+                              Feb 2024 - Aug 2024
+                            </p>
+                          </div>
+                        </div>
+
+                        <p className="mt-2 text-base sm:text-lg ">
+                          I contributed to the design, development, and testing
+                          of web-based applications by writing clean, efficient,
+                          and maintainable code. I collaborated with senior
+                          developers to troubleshoot issues, implement new
+                          features, and enhance existing systems using modern
+                          development tools and frameworks. I also participated
+                          in code reviews, team meetings, and agile workflows,
+                          gaining hands-on experience in full-stack development.
+                          Additionally, I supported documentation efforts and
+                          assisted in optimizing application performance to
+                          improve user experience.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
