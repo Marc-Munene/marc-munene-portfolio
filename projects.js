f6fce8e0c2bda94eab7768e9530c4014f1ff5efe
Marc Munene <marcmunene104@gmail.com>
2025-06-19 12:37:58 +0300

    projects update

--- projects.js
+++ projects.js
@@ -7,13 +7,13 @@ export const projectData = [
       "Class-Space is an intelligent web application designed to help students and class representatives easily locate and book vacant classrooms in real-time. It aims to eliminate the hassle of manual room allocation and prevent double bookings, making room management more efficient and organized.",
     link: "https://class-space.vercel.app/",
   },
-  {
-    title: "Chama",
-    image: "/chama.png",
-    description:
-      "Chama is a web-based platform designed to simplify and streamline group contributions and savings. Built with a user-friendly interface, it allows members to securely contribute funds, track group balances, view transaction history, and manage group goals in real-time.",
-    link: "http://chama-blue.vercel.app/",
-  },
+  // {
+  //   title: "Chama",
+  //   image: "/chama.png",
+  //   description:
+  //     "Chama is a web-based platform designed to simplify and streamline group contributions and savings. Built with a user-friendly interface, it allows members to securely contribute funds, track group balances, view transaction history, and manage group goals in real-time.",
+  //   link: "http://chama-blue.vercel.app/",
+  // },
   {
     title: "Kladi Outfits",
     image: "KLADI.png",
