54e4de20733957b61e80f91521baa6ca624883d4
Marc Munene <marcmunene104@gmail.com>
2026-03-29 10:18:40 +0300

    projects update

--- projects.js
+++ projects.js
@@ -1,5 +1,11 @@
-// projects.js
 export const projectData = [
+  {
+    title: "Sanlam Allianz Sales Portal",
+    image: "/SAGI.png",
+    description:
+      "End-to-end  enterprise sales portal. This mission-critical platform serves as the central hub for insurance agents and branches across the country, streamlining core insurance operations including new business processing, policy renewals, and vehicle valuations.",
+    link: "https://gisales.ke.sanlamallianz.com/",
+  },
   {
     title: "Class-Space",
     image: "/class-space.png",
@@ -7,13 +13,13 @@ export const projectData = [
       "Class-Space is an intelligent web application designed to help students and class representatives easily locate and book vacant classrooms in real-time. It aims to eliminate the hassle of manual room allocation and prevent double bookings, making room management more efficient and organized.",
     link: "https://class-space.vercel.app/",
   },
-  // {
-  //   title: "Chama",
-  //   image: "/chama.png",
-  //   description:
-  //     "Chama is a web-based platform designed to simplify and streamline group contributions and savings. Built with a user-friendly interface, it allows members to securely contribute funds, track group balances, view transaction history, and manage group goals in real-time.",
-  //   link: "http://chama-blue.vercel.app/",
-  // },
+  {
+    title: "Chama",
+    image: "/chama.png",
+    description:
+      "Chama is a web-based platform designed to simplify and streamline group contributions and savings. Built with a user-friendly interface, it allows members to securely contribute funds, track group balances, view transaction history, and manage group goals in real-time.",
+    link: "http://chama-blue.vercel.app/",
+  },
   {
     title: "Kladi Outfits",
     image: "KLADI.png",
@@ -21,11 +27,11 @@ export const projectData = [
       "A full-featured e-commerce platform with real-time inventory management, payment processing, and admin dashboard.",
     link: "https://kladi-outfits.vercel.app/",
   },
-  {
-    title: "CEMA Health Program",
-    image: "/cema.png",
-    description:
-      "A health program designed to add health programs by doctors, register patients and enroll them in different programs.",
-    link: "https://cema-health.vercel.app/",
-  },
+  // {
+  //   title: "CEMA Health Program",
+  //   image: "/cema.png",
+  //   description:
+  //     "A health program designed to add health programs by doctors, register patients and enroll them in different programs.",
+  //   link: "https://cema-health.vercel.app/",
+  // },
 ];
